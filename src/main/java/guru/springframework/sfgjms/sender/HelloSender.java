fd9029a214bc3419404fab56500e40c34eaf5b40
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-22 20:57:08 +0100

    added method sendAndReceive on a different queue

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.util.UUID;
 
 @Component
@@ -14,19 +20,38 @@ import java.util.UUID;
 public class HelloSender {
 
   private final JmsTemplate jmsTemplate;
+  private final ObjectMapper objectMapper;
 
   @Scheduled(fixedRate = 2000)
   public void sendMessage() {
-    System.out.println("Sending a message...");
-
     HelloWorldMessage message = HelloWorldMessage.builder()
         .id(UUID.randomUUID())
         .message("Hello world")
         .build();
 
     jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+  }
 
-    System.out.println("Message sent!");
+  @Scheduled(fixedRate = 2000)
+  public void sendAndReceiveMessage() throws JMSException {
+    HelloWorldMessage message = HelloWorldMessage.builder()
+        .id(UUID.randomUUID())
+        .message("Hello Alex!")
+        .build();
+
+    Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RVC_QUEUE, (MessageCreator) session -> {
+      Message helloMessage = null;
+      try {
+        helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+        helloMessage.setStringProperty("_type", HelloWorldMessage.class.getName());
+        System.out.println("Sending hello...");
+        return helloMessage;
+      } catch (JsonProcessingException e) {
+        throw new JMSException("exploded...");
+      }
+    });
+
+    System.out.println(receivedMsg.getBody(String.class));
   }
 
 }
